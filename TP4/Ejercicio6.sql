8b11f8f787b3dea14aef1e000e9f804815525d78
simongonzalez158 <simongonzalez158@gmail.com>
2024-10-10 11:53:31 -0300

    Subir cambios de TP3 y TP4 desde notebook

--- TP4/Ejercicio6.sql
+++ TP4/Ejercicio6.sql
@@ -1,5 +1,5 @@
 CREATE VIEW ciudad_kp_2 AS
-SELECT id_ciudad, nombre_ciudad, c.id_pais, nombre_pais
+SELECT c.id_ciudad, c.nombre_ciudad, c.id_pais, p.nombre_pais
 FROM unc_esq_peliculas.ciudad c NATURAL JOIN unc_esq_peliculas.pais p;
 
 CREATE VIEW entregas_kp_3 AS
@@ -42,9 +42,106 @@ BEGIN
 
     RETURN NEW;
 end;
-$$ language 'plpglsql';
+$$ language 'plpgsql';
 
 CREATE TRIGGER tr_insertar_ciudad_kp_2
 INSTEAD OF INSERT ON ciudad_kp_2
 FOR EACH ROW EXECUTE FUNCTION fn_insertar_ciudad_kp_2();
 
+-- -- -- -- -- -- --
+-- a
+
+CREATE OR REPLACE FUNCTION fn_update_ciudad() -- PREGUNTAR
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE ciudad
+    SET id_ciudad = new.id_ciudad,
+        nombre_ciudad = new.nombre_ciudad,
+        id_pais= new.id_pais
+        --nombre_ciudad = NEW.nombre_ciudad
+    WHERE id_ciudad = OLD.id_ciudad;
+
+    RETURN NEW;
+end;
+$$ language plpgsql;
+
+CREATE TRIGGER tr_update_ciudad
+INSTEAD OF UPDATE ON ciudad_kp_2
+FOR EACH ROW EXECUTE FUNCTION fn_update_ciudad();
+
+CREATE OR REPLACE FUNCTION fn_delete_ciudad()
+RETURNS TRIGGER AS $$
+BEGIN
+    DELETE FROM ciudad
+    WHERE id_ciudad = OLD.id_ciudad;
+
+    RETURN OLD;
+end;
+$$ language plpgsql;
+
+CREATE TRIGGER tr_delete_ciudad
+INSTEAD OF DELETE ON ciudad_kp_2
+FOR EACH ROW EXECUTE FUNCTION fn_delete_ciudad();
+
+CREATE OR REPLACE FUNCTION fn_insert_ciudad()
+RETURNS TRIGGER AS $$
+BEGIN
+    insert into ciudad(id_ciudad, nombre_ciudad, id_pais)
+    VALUES (new.id_ciudad, new.nombre_ciudad, new.id_pais);
+
+    RETURN new;
+    end;
+$$ language plpgsql;
+
+CREATE TRIGGER tr_insert_ciudad
+INSTEAD OF INSERT ON ciudad_kp_2
+FOR EACH ROW EXECUTE FUNCTION fn_insert_ciudad();
+
+-- b
+CREATE OR REPLACE FUNCTION fn_actualizar_entrega()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE renglon_entrega
+    SET cantidad = NEW.cantidad,
+        nro_entrega = NEW.nro_entrega,
+        codigo_pelicula = NEW.codigo_pelicula
+    WHERE nro_entrega = old.nro_entrega and codigo_pelicula = old.codigo_pelicula;
+
+    RETURN NEW;
+    end;
+$$ language plpgsql;
+
+CREATE TRIGGER tr_actualizar_ciudad
+INSTEAD OF UPDATE ON entregas_kp_3
+FOR EACH ROW EXECUTE FUNCTION fn_actualizar_entrega();
+
+CREATE OR REPLACE FUNCTION fn_insertar_entrega()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO renglon_entrega(nro_entrega, codigo_pelicula, cantidad)
+    VALUES (new.NRO_ENTREGA, new.codigo_pelicula, new.cantidad);
+
+    RETURN NEW;
+end;
+$$ language plpgsql;
+
+CREATE TRIGGER tr_insertar_entrega
+INSTEAD OF INSERT ON entregas_kp_3
+FOR EACH ROW EXECUTE FUNCTION fn_insertar_entrega();
+
+CREATE OR REPLACE FUNCTION fn_delete_entrega()
+RETURNS TRIGGER AS $$
+BEGIN
+    delete from renglon_entrega
+    WHERE nro_entrega = old.nro_entrega and codigo_pelicula = old.codigo_pelicula;
+
+    RETURN OLD;
+end;
+$$ language plpgsql;
+
+CREATE TRIGGER tr_delete_entrega
+INSTEAD OF DELETE ON entregas_kp_3
+FOR EACH ROW EXECUTE FUNCTION fn_delete_entrega();
+
+SELECT *
+FROM entregas_kp_3;
--- TP4/Ejercicio7.sql
+++ TP4/Ejercicio7.sql
@@ -1,4 +1,189 @@
-CREATE VIEW vista1 AS
+CREATE TABLE IF NOT EXISTS institucion AS
+    SELECT * FROM unc_esq_voluntario.institucion;
+
+CREATE TABLE IF NOT EXISTS direccion AS
+    SELECT * FROM unc_esq_voluntario.direccion;
+
+CREATE VIEW vista1_vol AS
     SELECT nro_voluntario, nombre, apellido, horas_aportadas, t.*
     FROM voluntario v JOIN tarea t USING(id_tarea);
 
+CREATE OR REPLACE FUNCTION fn_insert_vol_tarea()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO voluntario (nombre, apellido, e_mail, telefono, fecha_nacimiento, id_tarea, nro_voluntario, horas_aportadas, porcentaje, id_institucion, id_coordinador)
+    VALUES (NEW.nombre, NEW.apellido, new.e_mail, new.telefono,  new.fecha_nacimiento, NEW.id_tarea, NEW.nro_voluntario, NEW.horas_aportadas, NEW.porcentaje, NEW.id_institucion, NEW.id_coordinador);
+
+    RETURN NEW;
+    end;
+$$ language plpgsql;
+
+CREATE TRIGGER tr_datos_vol_tarea
+INSTEAD OF INSERT ON vista1_vol
+FOR EACH ROW EXECUTE FUNCTION fn_insert_vol_tarea();
+
+CREATE OR REPLACE FUNCTION fn_actualizar_vol_tarea()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE voluntario
+    SET nombre = new.nombre,
+        apellido = new.apellido,
+        e_mail = new.e_mail,
+        telefono = new.telefono,
+        fecha_nacimiento = new.fecha_nacimiento,
+        id_tarea = new.id_tarea,
+        nro_voluntario = new.nro_voluntario,
+        horas_aportadas = new.horas_aportadas,
+        porcentaje = new.porcentaje,
+        id_institucion = new.id_institucion,
+        id_coordinador = new.id_coordinador
+    WHERE nro_voluntario = OLD.nro_voluntario;
+
+    RETURN new;
+end;
+$$ language plpgsql;
+
+CREATE TRIGGER tr_actualizar_vol_tarea
+INSTEAD OF UPDATE ON vista1_vol
+FOR EACH ROW EXECUTE FUNCTION fn_actualizar_vol_tarea();
+
+CREATE OR REPLACE FUNCTION fn_delete_vol_tarea()
+RETURNS TRIGGER AS $$
+BEGIN
+    DELETE FROM voluntario
+    WHERE nro_voluntario = old.nro_voluntario;
+
+    DELETE FROM institucion
+    WHERE id_institucion = old.id_institucion; -- falta alguna clave?
+
+    RETURN old;
+    end;
+$$ language plpgsql;
+
+CREATE TRIGGER tr_delete_vol_tarea
+INSTEAD OF DELETE ON vista1_vol
+FOR EACH ROW EXECUTE FUNCTION fn_delete_vol_tarea();
+
+CREATE VIEW vista2_vol AS
+SELECT i.id_institucion, i.nombre_institucion,
+       (SELECT COUNT(DISTINCT v.id_tarea)
+        FROM voluntario v
+        WHERE v.id_institucion = i.id_institucion) AS total_tareas_realizadas
+FROM institucion i
+WHERE i.id_direccion IN(
+    SELECT d.id_direccion
+    FROM direccion d
+    );
+
+CREATE VIEW vista2_vol AS
+    SELECT i.*, COUNT(distinct v.id_tarea) AS Cant_tareas_realizadas
+    FROM institucion i JOIN voluntario v ON (i.id_institucion = v.id_institucion)
+        JOIN direccion d ON (i.id_direccion = d.id_direccion)
+        WHERE d.id_pais = 'US'
+    GROUP BY i.id_institucion, i.nombre_institucion, i.id_director, i.id_direccion;
+
+CREATE OR REPLACE FUNCTION fn_insert_vista2()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO institucion(id_institucion, nombre_institucion, id_director, id_director)
+    VALUES (NEW.id_institucion, NEW.nombre_institucion, NEW.id_director, NEW.id_director);
+
+    RETURN NEW;
+end;
+$$ language plpgsql;
+
+CREATE TRIGGER tr_insert_vista2
+INSTEAD OF INSERT on vista2_vol
+FOR EACH ROW EXECUTE FUNCTION fn_insert_vista2();
+
+CREATE OR REPLACE FUNCTION fn_actualizar_vista2()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE institucion
+    SET id_institucion = NEW.id_institucion,
+        nombre_institucion = NEW.nombre_institucion,
+        id_director = NEW.id_director,
+        id_direccion = NEW.id_direccion
+    WHERE id_institucion = OLD.id_institucion;
+
+    RETURN NEW;
+end;
+$$ language plpgsql;
+
+CREATE TRIGGER tr_actualizar_vista2
+INSTEAD OF UPDATE on vista2_vol
+FOR EACH ROW EXECUTE FUNCTION fn_actualizar_vista2();
+
+CREATE OR REPLACE FUNCTION fn_eliminar_vista2()
+RETURNS TRIGGER AS $$
+BEGIN
+    DELETE FROM institucion
+    WHERE id_institucion = OLD.id_institucion;
+
+    RETURN OLD;
+end;
+$$ language plpgsql;
+
+CREATE TRIGGER tr_eliminar_vista2
+INSTEAD OF DELETE on vista2_vol
+FOR EACH ROW EXECUTE FUNCTION fn_eliminar_vista2();
+
+CREATE VIEW vista3_vol AS
+    SELECT i.id_institucion, i.nombre_institucion, COUNT(*) as cant_voluntarios
+    FROM voluntario JOIN institucion i USING (id_institucion)
+    GROUP BY i.id_institucion, i.nombre_institucion;
+
+CREATE OR REPLACE FUNCTION fn_insertar_vista3()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO institucion(id_institucion, nombre_institucion, id_director, id_direccion)
+    VALUES (NEW.id_institucion, NEW.nombre_institucion, NEW.id_director, NEW.id_direccion);
+
+    RETURN NEW;
+end;
+$$ language plpgsql;
+
+CREATE TRIGGER tr_insertar_vista3
+INSTEAD OF INSERT on vista3_vol
+FOR EACH ROW EXECUTE FUNCTION fn_insertar_vista3();
+
+CREATE OR REPLACE FUNCTION fn_actualizar_vista3()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE institucion
+    SET id_institucion = NEW.id_institucion,
+        nombre_institucion = NEW.nombre_institucion,
+        id_director = NEW.id_director,
+        id_direccion = NEW.id_direccion
+    WHERE id_institucion = OLD.id_institucion;
+
+    RETURN NEW;
+    end;
+$$ language plpgsql;
+
+CREATE TRIGGER tr_actualizar_vista3
+INSTEAD OF INSERT on vista3_vol
+FOR EACH ROW EXECUTE FUNCTION fn_actualizar_vista3();
+
+CREATE OR REPLACE FUNCTION fn_eliminar_vista3()
+RETURNS TRIGGER AS $$
+BEGIN
+    DELETE FROM institucion
+    WHERE id_institucion = OLD.id_institucion;
+
+    RETURN OLD;
+end;
+$$ language plpgsql;
+
+CREATE TRIGGER tr_eliminar_vista3
+INSTEAD OF INSERT on vista3_vol
+FOR EACH ROW EXECUTE FUNCTION fn_eliminar_vista3();
+
+SELECT *
+FROM vista1_vol;
+
+SELECT *
+FROM vista2_vol;
+
+SELECT *
+FROM vista3_vol;
